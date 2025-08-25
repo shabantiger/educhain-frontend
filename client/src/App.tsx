6b76f567b9b1fdad4eac79b0d1e73d93e5784e8e
shabantiger <magidutyga@gmail.com>
2025-08-25 16:25:07 +0300

    vercel insights intergrated

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -3,6 +3,8 @@ import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
 import { TooltipProvider } from "@/components/ui/tooltip";
+import { Analytics } from "@vercel/analytics/react";
+import { SpeedInsights } from "@vercel/speed-insights/react";
 import Layout from "@/components/Layout";
 import ProtectedRoute from "@/components/ProtectedRoute";
 import Landing from "@/pages/landing";
@@ -77,6 +79,8 @@ function App() {
       <TooltipProvider>
         <Toaster />
         <Router />
+        <Analytics />
+        <SpeedInsights />
       </TooltipProvider>
     </QueryClientProvider>
   );
